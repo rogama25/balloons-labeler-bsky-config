14b4f9b1070b1eb4b9de1e43b6ce1ef41957b60d
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2025-12-13 15:37:31 +0100

    Oauth implementation

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: true
 };
 
 const withNextIntl = createNextIntlPlugin();
