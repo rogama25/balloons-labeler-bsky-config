c148d25da32525f9ab64f45d4e4901efdf59d30c
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2025-12-13 14:43:57 +0100

    Oauth implementation

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,14 @@
 import {Sequelize} from "sequelize-typescript";
 import "dotenv/config"
 import {BlueskyLoginState, BlueskySession, User} from "@/types/db";
+import pg from "pg";
 
 export {};
 
-export const db = new Sequelize(process.env.DB || "");
+export const db = new Sequelize(process.env.DB || "", {
+  dialect: "postgres",
+  dialectModule: pg
+});
 
 db.addModels([
   User,
