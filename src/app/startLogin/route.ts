e6c8fb5c7612d9a6017082e88b6760da98e19437
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2025-12-14 14:32:18 +0100

    fix

--- src/app/startLogin/route.ts
+++ src/app/startLogin/route.ts
@@ -3,7 +3,7 @@ import "@/config/db"
 import {NextRequest} from "next/server";
 
 export async function GET(request: NextRequest) {
-  const handle = "rogama25.es";
+  const handle = request.nextUrl.searchParams.get("handle") || "";
   const redirectUrl = await startAuth(handle);
   return Response.redirect(redirectUrl);
 }
